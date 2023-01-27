2dab2aaaf733e6f0cb609ec841b6f49414bdf7c7
Emmanuel Gonzales <emmanuelgonzales2002@gmail.com>
2023-01-27 14:13:53 -0600

    To fix lab 06

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
 console.log("secret console messege");
 
-
 function birthdaySurprise(){
     let usersName = prompt("What's Your Name");
     
     let input = prompt("Is Today Your Birthday");
-    
+       
     if (input == "yes") {
+      let age = prompt("What age are you today?");
       document.write("Happy Birthday ");
       document.write(usersName);
-      document.write("!! If you sign up to our news letter you can recive Yearly Birthday Discounts");
+      document.write("!! If you sign up to our news letter you can recive a " + age + "% Discount on your next purchase");
+      
     }
      else if (input == "Yes") {
-        document.write("Happy Birthday ");
-        document.write(usersName);
-        document.write("!! If you sign up to our news letter you can recive Yearly Birthday Discounts");
+      let age = prompt("What age are you today?");
+      document.write("Happy Birthday ");
+      document.write(usersName);
+      document.write("!! If you sign up to our news letter you can recive a " + age + "% Discount on your next purchase");;
     } 
-     else if (input == "YES") {
-        document.write("Happy Birthday ");
-        document.write(usersName);
-        document.write("!! If you sign up to our news letter you can recive Yearly Birthday Discounts");
-    }
-     
-    else if (input == "no"){
+     else if (input == "no"){
+        let age = prompt("How old will you be on your next birthday?");
         document.write("That's to bad ");
         document.write(usersName);
-        document.write(", come back on your birthday for a special surprise");
-    } 
+        document.write(", come back on your "  + age + " birthday for a special surprise");
+    }    
      else if (input == "No"){
+        let age = prompt("How old will you be on your next birthday?");
         document.write("That's to bad ");
         document.write(usersName);
-        document.write(", come back on your birthday for a special surprise");
+        document.write(", come back on your "  + age + " birthday for a special surprise");
     }    
      else if (input == "NO"){
+        let age = prompt("How old will you be on your next birthday?");
         document.write("That's to bad ");
         document.write(usersName);
-        document.write(", come back on your birthday for a special surprise");
+        document.write(", come back on your "  + age + " birthday for a special surprise");
     }    
      else if (input == "potato"){confirm("yummy");}
      else if (input == "Potato"){confirm("Yummy");}
